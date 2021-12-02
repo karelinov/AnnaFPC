c610cc889481656f2f12401fdf99ae8e9152c241
karelinov@gmail.com <karelinov@gmail.com>
2021-12-02 20:24:00 +0300

    +

---
+++ Project1/Program.pas
@@ -0,0 +1,15 @@
+program Test;
+
+uses xxx, sysutils;
+
+var a:string;
+var b:integer;
+begin
+  Writeln('sdfsdaf');
+  Readln(a);
+  Writeln('a='+a);
+  b:=xxx.xxx(12);
+  Writeln('b='+IntToStr(b));
+  readln()
+end.
+
---
+++ Project1/xxx.pas
@@ -0,0 +1,11 @@
+unit xxx;
+interface
+function xxx(input:integer):integer;
+
+implementation
+function xxx(input:integer):integer;
+begin
+  xxx:=input;   
+end; 
+
+end.
